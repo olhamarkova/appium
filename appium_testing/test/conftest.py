180b8c13cfd5412644c2efa6533db5dae032ce19
olhamarkova <olga.mulchenko@gmail.com>
2025-04-06 19:35:22 +0300

    add conftest. move fixtures to conftest

---
+++ appium_testing/test/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from contextlib import contextmanager
+from appium_testing.utils.server_launching import start_appium_server, stop_appium_server
+from appium_testing.utils.desired_caps import app_capabilities, load_capabilities
+
+@pytest.fixture(scope="module")
+def setup():
+    start_appium_server()
+
+    yield
+    stop_appium_server()
+
+
+@pytest.fixture(scope="function")
+def app_factory():
+    @contextmanager
+    def _create_app(app_class, caps: dict):
+        desired_caps = app_capabilities(**caps)
+        capabilities_options = load_capabilities(desired_caps)
+        app_instance = app_class(capabilities_options)
+        try:
+            yield app_instance
+        finally:
+            app_instance.quit()
+
+    return _create_app
--- appium_testing/test/test_add_contact.py
+++ appium_testing/test/test_add_contact.py
@@ -1,35 +1,23 @@
 import pytest
-from appium_testing.utils.server_launching import start_appium_server, stop_appium_server
-from appium_testing.utils.desired_caps import app_capabilities, load_capabilities
 from appium_testing.apps.contacts.contacts_app import ContactsApp
 
-@pytest.fixture(scope="module")
-def setup():
-    start_appium_server()
-
-    yield
-    stop_appium_server()
-
-@pytest.fixture(scope="function")
-def contact_app():
-    desired_caps = app_capabilities(appPackage="com.google.android.dialer",
-                                    appActivity=".extensions.GoogleDialtactsActivity")
-    capabilities_options = load_capabilities(desired_caps)
-    contact = ContactsApp(capabilities_options)
-    yield contact
-    contact.quit()
+contact_app_capabilities = {
+    "appPackage": "com.google.android.dialer",
+    "appActivity": ".extensions.GoogleDialtactsActivity",
+}
 
 @pytest.mark.usefixtures("setup")
 @pytest.mark.functional
-def test_add_new_contact(contact_app):
-    contact_app.open_contacts_tab()
-    number_of_contacts_before = contact_app.get_numbers_count()
-    contact_app.open_new_contact_form()
-    contact_app.wait(1)
-    contact_app.enter_name("First name", "John")
-    contact_app.enter_name("Last name", "Doe")
-    contact_app.enter_number("234567891")
-    contact_app.save_number()
-    number_of_contacts_after = contact_app.get_numbers_count()
+def test_add_new_contact(app_factory):
+    with app_factory(ContactsApp, contact_app_capabilities) as contact_app:
+        contact_app.open_contacts_tab()
+        number_of_contacts_before = contact_app.get_numbers_count()
+        contact_app.open_new_contact_form()
+        contact_app.wait(1)
+        contact_app.enter_name("First name", "John")
+        contact_app.enter_name("Last name", "Doe")
+        contact_app.enter_number("234567891")
+        contact_app.save_number()
+        number_of_contacts_after = contact_app.get_numbers_count()
 
-    assert number_of_contacts_before + 1 == number_of_contacts_after
+        assert number_of_contacts_before + 1 == number_of_contacts_after
